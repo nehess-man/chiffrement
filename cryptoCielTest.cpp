9a7e946c3ac99606007414c161c2a5fa33bcd654
nehess-man <valetertre2856@gmail.com>
2024-09-19 11:08:21 +0200

    Add files via upload

---
+++ cryptoCielTest.cpp
@@ -0,0 +1,17 @@
+// RsaCiel.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+#include "RsaGestion.h"
+#include "Hashgestion.h"
+#include "AesGestion.h"
+
+int main()
+{
+	HashGestion LM;
+	std::string File = "test.txt";
+	std::cout << LM.CalculateFileSHA256(File) << std::endl;
+
+}
+
+
